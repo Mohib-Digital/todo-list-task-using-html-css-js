573c20568656e5ce55b4d5b20a7b48f40aedb43b
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-09-22 15:32:12 +0500

    create li and append li into ul

--- scripts.js
+++ scripts.js
@@ -6,10 +6,24 @@ const showTask = document.getElementById('showTask');
 
 
 addTask.addEventListener('click', () => {
-
+// create condition for empty value
   if (!textInput.value.trim()) {
    alert('Please type todo task!')
+   return; // Stop further execution if input is empty
   }
+
+  // create an li element
+  const li = document.createElement('li');
+  li.textContent = textInput.value;
+  
+
+  // append li in ul element
+  showTask.append(li);
+  li.classList.add('liElement');
+
+
+
+
 });
 
 
