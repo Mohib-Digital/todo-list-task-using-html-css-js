24464c073ceec3262a6105d8005373b3461ffbae
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-09-22 13:52:38 +0500

    modified html css, create logic for input empty spacing, using js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 const textInput = document.getElementById('textInput');
 const addTask = document.getElementById('addTask');
-const showTask = document.getElementById('showTask')
+const showTask = document.getElementById('showTask');
+
+
+
+
+addTask.addEventListener('click', () => {
+
+  if (!textInput.value.trim()) {
+   alert('Please type todo task!')
+  }
+});
+
+
