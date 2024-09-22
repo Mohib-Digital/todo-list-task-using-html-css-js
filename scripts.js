14a84263570a9a862c9540d6a23ad6fd8504eba9
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-09-22 16:06:43 +0500

    create remove btn, append li and remove btn into ul

--- scripts.js
+++ scripts.js
@@ -15,19 +15,23 @@ addTask.addEventListener('click', () => {
   // create an li element
   const li = document.createElement('li');
   li.textContent = textInput.value;
+  li.classList.add('liElement');
   
 
-  // append li in ul element
-  showTask.append(li);
-  li.classList.add('liElement');
-
   // add & remove linethrough
-  li.addEventListener("click",function(){
-    li.classList.toggle("liToggle")
-  })
+    li.addEventListener("click",function(){
+      li.classList.toggle("liToggle")
+    })
+ 
+
+// Remove button code start here
+  const removeBtn = document.createElement("button")
+   removeBtn.textContent = "Remove"
 
 
-
+  // append li in ul element
+  li.append(removeBtn);
+  showTask.append(li);
 });
 
 
